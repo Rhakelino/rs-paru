a22a12d12a35c153acd572389a8645e86891893a
rakel lino <instanclay@gmail.com>
2025-02-24 14:45:17 +0700

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import Home from "./pages/Home";  // Pastikan file ini ada di /src/pages/Home.jsx
-import Visi from "./pages/Visi";  // Pastikan file ini ada di /src/pages/Visi.jsx
+import Home from './Pages/Home';
+import Visi from './Pages/Visi';
+
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Route untuk halaman Home */}
         <Route path="/" element={<Home />} />
-        {/* Route untuk halaman Visi */}
         <Route path="/visi" element={<Visi />} />
       </Routes>
     </Router>
